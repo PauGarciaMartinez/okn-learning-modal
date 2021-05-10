a8d3b51ab16327e57b40243237769cfe42f2ba77
Pau Garcia Martínez <etpau@hotmail.es>
2021-05-10 14:00:10 +0200

    Add profile photos for student and teacher

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,18 +1,25 @@
 <template>
+
   <div v-for="msg in messages" :key="msg">
     <div v-if="msg.value.author === 'student'" class="messages-wrapper">
       <div class="message-container">
         <p>{{ msg.value.content }}</p>
         <small>{{ msg.value.date }}</small>
       </div>
+      <div class="thumbnail">
+        <img src="@/assets/student-profile.jpeg" alt="Student's profile photo">
+      </div>
     </div>
     <div v-else class="messages-wrapper2">
+      <div class="thumbnail">
+        <img src="@/assets/teacher-profile.jpeg" alt="Teacher's profile photo">
+      </div>
       <div class="message-container2">
         <p>{{ msg.value.content }}</p>
         <small>{{ msg.value.date }}</small>
       </div>
     </div>
-
+    
   </div>
   
 </template>
@@ -45,9 +52,10 @@ export default {
 /* STUDENT MESSAGES */
 .messages-wrapper {
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-end;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 1rem;
   margin: 1rem 0;
 }
 .message-container {
@@ -59,12 +67,23 @@ export default {
   color: white;
 }
 
+.thumbnail {
+  max-width: 50px;
+}
+
+.thumbnail img {
+  max-height: 30px;
+  max-width: 35px;
+  border-radius: 0.5rem;
+}
+
 /* TEACHER MESSAGES */
 .messages-wrapper2 {
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 1rem;
   margin: 1rem 0;
 }
 .message-container2 {
